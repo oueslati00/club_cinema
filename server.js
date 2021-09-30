030160456857ee5b4aded96c9cd4b5bf37270e91
BRAHIM Oueslati <boueslati76@gmail.com>
2021-09-30 17:49:54 +0100

    add disct folder to project

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static('./dist/'));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/'}),
+);
+app.listen(process.env.PORT || 8080);
