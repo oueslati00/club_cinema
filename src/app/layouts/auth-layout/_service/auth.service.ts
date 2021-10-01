b7d7a8fc7ea7810df3ea8d80637fe5f46c0237f4
BRAHIM Oueslati <boueslati76@gmail.com>
2021-10-01 14:59:51 +0100

    fix bug envriment.url2

--- src/app/layouts/auth-layout/_service/auth.service.ts
+++ src/app/layouts/auth-layout/_service/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {environment} from '../../../../environments/environment.prod';
 
-const AUTH_API = environment + 'api/auth/';
+const AUTH_API = environment.url + 'api/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -18,7 +18,7 @@ export class AuthService {
 
   login(credentials): Observable<any> {
     console.log(credentials);
-    return this.http.post(environment + 'api/auth/signin', {
+    return this.http.post(environment.url + 'api/auth/signin', {
       username: credentials.username,
       password: credentials.password
     }, httpOptions);
